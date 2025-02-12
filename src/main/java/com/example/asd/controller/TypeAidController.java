ff6fd5e721a8487a84754dfe67196815cdf14556
farhat <farhatsalma238@gmail.com>
2025-02-12 09:07:59 +0100

    reformulation du controller TypeAidController selon ApiResponse

--- src/main/java/com/example/asd/controller/TypeAidController.java
+++ src/main/java/com/example/asd/controller/TypeAidController.java
@@ -1,11 +1,15 @@
 package com.example.asd.controller;
 
+import com.example.asd.dto.response.ApiResponse;
 import com.example.asd.dto.response.TypeAidResponse;
+import com.example.asd.exception.DomainException;
 import com.example.asd.service.TypeAidService;
+import jakarta.validation.constraints.NotNull;
 import org.apache.coyote.Response;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,9 +26,17 @@ public class TypeAidController {
     }
 
     @GetMapping
-    public ResponseEntity<List<TypeAidResponse>> getAllTypeAids() {
-        return ResponseEntity.ok()
-                .contentType(MediaType.APPLICATION_JSON)
-                .body(typeAidService.getAllTypeAid());
+    public ResponseEntity<ApiResponse<?>> getAllTypeAids() {
+        try {
+            return ResponseEntity.ok(new ApiResponse<>(typeAidService.getAllTypeAid()));
+        } catch (DomainException e) {
+            return ResponseEntity.badRequest().body(ApiResponse.of(null, "400", e.getMessage()));
+        }
     }
+
+//    public ResponseEntity<List<TypeAidResponse>> getAllTypeAids() {
+//        return ResponseEntity.ok()
+//                .contentType(MediaType.APPLICATION_JSON)
+//                .body(typeAidService.getAllTypeAid());
+//    }
 }
